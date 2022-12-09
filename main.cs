7081d882ad02da135add1891dd80ed9cb3af226a
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-12-09 14:38:58 -0500

    c#

--- main.cs
+++ main.cs
@@ -8,12 +8,12 @@ class Program
 {
     public static void Main(string[] args)
     {
-      string text = null;
-      var counter = 0;
-      int integerA;
-      int integerB;
-      int integerBOV;
-        
+        string text = null;
+        var counter = 0;
+        int integerA;
+        int integerB;
+        int integerBOV;
+
         Console.Write("Repeated addition using while loop.");
         Console.Write("\nEnter integer A: ");
         integerA = Convert.ToInt32(Console.ReadLine());
@@ -26,7 +26,6 @@ class Program
         counter++;
         integerB = integerB + integerBOV;
         }
-
         Console.Write(text);
         Console.Write("\nDone");
     }
